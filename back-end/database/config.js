501fb1896c4631dd624f7e85147e0a12fb81e35a
Toni Salminen <toni.salminen89@gmail.com>
2022-01-05 04:31:32 +0200

    add dummy config-data, edit connectionFunctions

---
+++ back-end/database/config.js
@@ -0,0 +1,7 @@
+const config = {
+    host: "localhost",
+    user: "username",
+    password:"password"
+};
+
+module.exports = config;
--- back-end/database/crud.js
+++ back-end/database/crud.js
@@ -1,12 +1,24 @@
 const mysql = require('mysql');
+const config = require("./config.js");
 
 let connection = null;
 
 const connectionFunctions = {
     connect: () => {
-        return new Promise((resolve, reject) => {});
+        return new Promise((resolve, reject) => {
+            connection = mysql.createConnection(config);
+            connection
+                ? resolve("Connected!")
+                : reject(new Error("Connection failed."));
+        });
     },
     close: () => {
+        return new Promise((resolve) => {
+            connection.end();
+            resolve("Connection closed.");
+        });
+    },
+    getAll: () => {
         return new Promise((resolve, reject) => {});
     },
     search: () => {
