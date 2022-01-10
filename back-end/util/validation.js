6ad8fc7d956c8dd153aa4573c44e3fa06123ed08
Toni Salminen <toni.salminen89@gmail.com>
2022-01-10 03:42:38 +0200

    edit validation to match requirements

--- back-end/util/validation.js
+++ back-end/util/validation.js
@@ -2,13 +2,32 @@ const Validator = require("jsonschema").Validator;
 const validator = new Validator();
 
 const dataSchema = {
-    type: "object",
-    properties: {
-        farmName: {type: "string"},
-        date: {type: ["datetime", "date"]},
-        metricType: {type: "string"},
-        metricValue: {type: "number"},
+    "type": "object",
+    "properties": {
+        "name": {"type": "string"},
+        "date": {"type": ["datetime", "date"]},
+        "type": {"enum": ["temperature", "rainFall", "pH"]},
+        "value": {"type": "number"},
     },
+    "required": ["type"],
+    "if": {
+        "properties": {"type": {"const": "pH"}}
+    },
+    "then": {
+        "properties": {"value": {"minimum": 0, "maximum": 14}}
+    },
+    "if": {
+        "properties": {"type": {"const": "temperature"}}
+    },
+    "then": {
+        "properties": {"value": {"minimum": -50, "maximum": 100}}
+    },
+    "if": {
+        "properties": {"type": {"const": "rainFall"}}
+    },
+    "then": {
+        "properties": {"value": {"minimum": 0, "maximum": 500}}
+    }
 }
 
 const validation = {
@@ -16,6 +35,8 @@ const validation = {
         return validator.validate(data, dataSchema);
     }
 }
+
+console.log("test")
 
 module.exports = validation;
 
