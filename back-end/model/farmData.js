7320b06f5580f8be55d119852bfe1d418d4f4fa8
Toni Salminen <toni.salminen89@gmail.com>
2022-01-11 15:28:05 +0200

    dataModel for csv-farmData row

---
+++ back-end/model/farmData.js
@@ -0,0 +1,10 @@
+class FarmData {
+    constructor(data) {
+        this.name = data.name;
+        this.date = data.date;
+        this.type = data.type;
+        this.value = data.value;
+    }
+}
+
+module.exports = FarmData;
