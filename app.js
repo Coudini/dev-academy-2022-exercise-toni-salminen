c63d1a36ee22865d28b7ce83ba34300c2e2a3b8f
Toni Salminen <toni.salminen89@gmail.com>
2022-01-15 21:49:18 +0200

    edit for heroku

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ const apiRouter = require('./routes/farmData.js');
 
 app.use(express.static(path.resolve(__dirname, './build')));
 
+/*
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
+*/
 
 //app.use(apiRouter);
 
